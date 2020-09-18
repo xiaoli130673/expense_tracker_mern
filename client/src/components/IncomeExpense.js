8e7915b364be4a1e4ab6976c7241085d817d702a
Xiao Juan Li <xiaoli130673@gmail.com>
2020-09-18 13:59:54 -0400

    Update IncomeExpense.js

--- client/src/components/IncomeExpense.js
+++ client/src/components/IncomeExpense.js
@@ -36,18 +36,4 @@ const IncomeExpense = () => {
   );
 };
 
-// const amounts = transactions.map((transaction) => transaction.amount);
-// const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
-
-// const income = amounts
-//   .filter((item) => item > 0)
-//   .reduce((acc, item) => (acc += item), 0)
-//   .toFixed(2);
-
-// const expense =
-//   amounts
-//     .filter((item) => item < 0)
-//     .reduce((acc, item) => (acc += item), 0)
-//     .toFixed(2) * -1;
-
 export default IncomeExpense;
