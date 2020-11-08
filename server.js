bf253c55b618719461777536273f592c6a4c28f7
Xiao Juan Li <xiaoli130673@gmail.com>
2020-11-08 18:07:41 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,3 @@
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const colors = require('colors');
-// const connectDB = require('./config/db');
-
-// dotenv.config({ path: './config/config.env' });
-
-// connectDB();
-
-// const transactions = require('./routes/transactions');
-// const app = express();
-
-// app.use('/api/v1/transactions', transactions);
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(
-//   PORT,
-//   console.log(
-//     `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
-//       .yellow.bold
-//   )
-// );
 const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
